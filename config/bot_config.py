088b18f53b51db6cc095303626d50697275a854e
mr.Robot <zanid227@yandex.ru>
2024-11-25 17:17:47 +0300

    Начата разработка

--- config/bot_config.py
+++ config/bot_config.py
@@ -6,13 +6,25 @@ from environs import Env
 class TgBot:
     token: str
 
+@dataclass
+class Redis:
+    host: str
+    port: int
+    db: int
+    password: str
+
 
 @dataclass
 class ConfigBot:
     tg_bot: TgBot
+    redis: Redis
 
 
 def load_bot_config(path: str | None = None) -> ConfigBot:
     env = Env()
     env.read_env(path)
-    return ConfigBot(tg_bot=TgBot(token=env('BOT_TOKEN')))
+    return ConfigBot(tg_bot=TgBot(token=env('BOT_TOKEN')),
+                     redis=Redis(host=env('REDIS_HOST'),
+                                 port=int(env('REDIS_PORT')),
+                                 db=int(env('REDIS_DB')),
+                                 password=env('REDIS_PASSWORD')))
--- main.py
+++ main.py
@@ -2,13 +2,30 @@ import asyncio
 import logging
 
 from aiogram import Bot, Dispatcher
-
-
-logging.basicConfig(
-    filename='bot.log',
-    level=logging.DEBUG,
-    format='%(asctime)s - %(name)s - %(levelname)-8s %(filename)s:'
-           '%(lineno)d - %(message)s')
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
 
 logger = logging.getLogger(__name__)
+
+async def main():
+    logging.basicConfig(
+        filename='bot.log',
+        level=logging.DEBUG,
+        format='%(asctime)s - %(name)s - %(levelname)-8s %(filename)s:'
+               '%(lineno)d - %(message)s')
+
+    logger.info('Start bot')
+
+    # config: Config = load_config()
+
+    bot = Bot(token='7760292733:AAFpm_ffEpHNF_HdBNAGmKoEK3Gm3GgmlsQ',
+              default=DefaultBotProperties(parse_mode=ParseMode.MARKDOWN))
+    dp = Dispatcher()
+
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling()
+
+
+asyncio.run(main())
+
 
